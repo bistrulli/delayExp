6ab8d3f63e6b26903d6b5142f1cd2117fde5bdea
emilio <emilio@mpbwalt.local>
2022-09-22 17:17:01 +0200

    minor

--- N1/src/main/java/app/Main.java
+++ N1/src/main/java/app/Main.java
@@ -22,15 +22,18 @@ public class Main {
 
 	private static Boolean isEmu = false;
 	private static String dbHost = null;
-	private static boolean cgv2= false;
-	private static Integer port=3000;
+	private static boolean cgv2 = false;
+	private static Integer port = 3000;
 	private static String name;
+	private static Float alfa;
+	private static Float tgt;
+	private static Integer nr;
 
 	public static void main(String[] args) {
 		System.setProperty("net.spy.log.LoggerImpl", "net.spy.memcached.compat.log.SLF4JLogger");
-		Unirest.config().concurrency(2000,2000); 
+		Unirest.config().concurrency(2000, 2000);
 		Main.getCliOptions(args);
-		if(Main.cgv2) {
+		if (Main.cgv2) {
 			try {
 				Main.addToCgv2();
 			} catch (Exception e) {
@@ -42,18 +45,18 @@ public class Main {
 	}
 
 	public static void resetState(SimpleTask task) throws InterruptedException, ExecutionException {
-		MemcachedClient memcachedClient=null;
+		MemcachedClient memcachedClient = null;
 		try {
 			memcachedClient = new MemcachedClient(new InetSocketAddress(Main.dbHost, 11211));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		memcachedClient.set(task.getName()+"_sw", 3600,"1").get();
-		memcachedClient.set(task.getName()+"_hw", 3600,"1").get();
+		memcachedClient.set(task.getName() + "_sw", 3600, "1").get();
+		memcachedClient.set(task.getName() + "_hw", 3600, "1").get();
 		String[] entries = task.getEntries().keySet().toArray(new String[0]);
 		for (String e : entries) {
-			memcachedClient.set(e + "_bl",Integer.MAX_VALUE, "0").get();
-			memcachedClient.set(e + "_ex",Integer.MAX_VALUE,"0").get();
+			memcachedClient.set(e + "_bl", Integer.MAX_VALUE, "0").get();
+			memcachedClient.set(e + "_ex", Integer.MAX_VALUE, "0").get();
 		}
 		memcachedClient.shutdown();
 	}
@@ -62,57 +65,60 @@ public class Main {
 		HashMap<String, Class> t1Entries = new HashMap<String, Class>();
 		HashMap<String, Long> t1Entries_stimes = new HashMap<String, Long>();
 		t1Entries.put("e1", N1HTTPHandler.class);
-		t1Entries_stimes.put("e1", 200l);
-		final SimpleTask N1 = new SimpleTask("localhost", Main.port, t1Entries, t1Entries_stimes, 10, Main.isEmu, Main.name,
-				Main.dbHost,1l,1l,null,Main.cgv2);
+		t1Entries_stimes.put("e1", 50l);
+		final SimpleTask N1 = new SimpleTask("localhost", Main.port, t1Entries, t1Entries_stimes, 1, Main.isEmu,
+				Main.name, Main.dbHost, 1l, 1l, null, Main.cgv2);
 		N1.setHwCore(1f);
+		N1.getCtrl().setNr(Main.nr);
+		N1.getCtrl().setAlpha(Main.alfa);
+		N1.getCtrl().setTauro(Main.tgt);
+		
 		return new SimpleTask[] { N1 };
 	}
-	
+
 	public static boolean validate(final String hostname) {
-        return InetAddresses.isUriInetAddress(hostname) || 
-        		InternetDomainName.isValid(hostname);
-    }
-	
+		return InetAddresses.isUriInetAddress(hostname) || InternetDomainName.isValid(hostname);
+	}
+
 	private static Boolean isCgroupV2Enabled() {
-		File cgfile = new File("/sys/fs/cgroup/"+Main.name+"/cgroup.procs");
+		File cgfile = new File("/sys/fs/cgroup/" + Main.name + "/cgroup.procs");
 		return cgfile.exists();
 	}
-	
+
 	public static void addToCgv2() throws Exception {
-		if(Main.isCgroupV2Enabled()) {
-		try {
-			int tid = GetThreadID.get_tid();
-			// aggiungo questo thread al gruppo dei serventi del tier
-			BufferedWriter out;
+		if (Main.isCgroupV2Enabled()) {
 			try {
-				out = new BufferedWriter(new FileWriter("/sys/fs/cgroup/"+Main.name+"/cgroup.procs", true));
-				out.write(String.valueOf(tid));
-				out.flush();
-				out.close();
-			} catch (IOException e) {
+				int tid = GetThreadID.get_tid();
+				// aggiungo questo thread al gruppo dei serventi del tier
+				BufferedWriter out;
+				try {
+					out = new BufferedWriter(new FileWriter("/sys/fs/cgroup/" + Main.name + "/cgroup.procs", true));
+					out.write(String.valueOf(tid));
+					out.flush();
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		}else{
+		} else {
 			throw new Exception("Cgroupv2 is not enabled");
 		}
 	}
 
 	public static void getCliOptions(String[] args) {
-		
-		
 
 		int c;
-		LongOpt[] longopts = new LongOpt[5];
+		LongOpt[] longopts = new LongOpt[8];
 		longopts[0] = new LongOpt("cpuEmu", LongOpt.REQUIRED_ARGUMENT, null, 0);
 		longopts[1] = new LongOpt("dbHost", LongOpt.REQUIRED_ARGUMENT, null, 1);
 		longopts[2] = new LongOpt("cgv2", LongOpt.REQUIRED_ARGUMENT, null, 2);
 		longopts[3] = new LongOpt("port", LongOpt.REQUIRED_ARGUMENT, null, 3);
 		longopts[4] = new LongOpt("name", LongOpt.REQUIRED_ARGUMENT, null, 4);
-		
+		longopts[5] = new LongOpt("alfa", LongOpt.REQUIRED_ARGUMENT, null, 5);
+		longopts[6] = new LongOpt("tgt", LongOpt.REQUIRED_ARGUMENT, null, 6);
+		longopts[7] = new LongOpt("nr", LongOpt.REQUIRED_ARGUMENT, null, 7);
 
 		Getopt g = new Getopt("ddctrl", args, "", longopts);
 		g.setOpterr(true);
@@ -137,7 +143,7 @@ public class Main {
 				break;
 			case 2:
 				try {
-					Main.cgv2 = Integer.valueOf(g.getOptarg()) > 0 ? true : false; 
+					Main.cgv2 = Integer.valueOf(g.getOptarg()) > 0 ? true : false;
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -152,6 +158,27 @@ public class Main {
 			case 4:
 				try {
 					Main.name = String.valueOf(g.getOptarg());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			case 5:
+				try {
+					Main.alfa = Float.valueOf(g.getOptarg());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			case 6:
+				try {
+					Main.tgt = Float.valueOf(g.getOptarg());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			case 7:
+				try {
+					Main.nr = Integer.valueOf(g.getOptarg());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
