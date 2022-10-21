c0ffb7f5af4e63f3fcfac4cfdf7d4efe1d552521
emilio <emilio@mpbwalt.local>
2022-10-21 23:05:10 +0200

    minor

--- N1/start.sh
+++ N1/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa 0.95 --tgt 0.6 --nr 20
+java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa 0.95 --tgt 0.6 --nr 20
