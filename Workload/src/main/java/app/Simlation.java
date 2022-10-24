d72756e698f00ccfd91b1ffacd4d080d78ab00be
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-10-24 16:09:27 +0200

    data

--- Workload/src/main/java/app/Simlation.java
+++ Workload/src/main/java/app/Simlation.java
@@ -18,41 +18,56 @@ public class Simlation implements Runnable {
 	UniformIntegerDistribution dist = null;
 	ArrayList<Double> roi = null;
 	ArrayList<Double> ctime = null;
+	int[] rates = null;
 
 	public Simlation(SimpleTask generator, Integer toChange) {
 		this.generator = generator;
 		this.simStep = 0;
 		this.toChange = toChange;
 		this.roi = new ArrayList<Double>();
-		this.ctime= new ArrayList<Double>();
-		this.dist = new UniformIntegerDistribution(50, 300);
+		this.ctime = new ArrayList<Double>();
+		// this.dist = new UniformIntegerDistribution(50, 300);
+		this.rates = new int[] { 300, 50, 100, 200 };
+
 	}
 
 	public void run() {
 		this.simStep += 1;
-		System.out.println("step="+this.simStep);
+		System.out.println("step=" + this.simStep);
+		int r = 0;
 		if (this.simStep % this.toChange == 0) {
-			Integer rate = this.dist.sample();
-			System.out.println("new Rate="+rate);
-			this.roi.add(30.0/(rate.doubleValue()/1000.0));
+			// Integer rate = this.dist.sample();
+			Integer rate = this.rates[r];
+			System.out.println("new Rate=" + rate);
+			this.roi.add(30.0 / (rate.doubleValue() / 1000.0));
 			this.ctime.add(Long.valueOf(System.nanoTime()).doubleValue());
 			for (Client c : Client.getClients()) {
 				c.setThinkTime(rate.longValue());
 			}
-			
+
 			MatFile matFile = Mat5.newMatFile();
 			Matrix roiMatrix = Mat5.newMatrix(1, this.roi.size());
 			Matrix ctimeMatrix = Mat5.newMatrix(1, this.ctime.size());
 			for (int i = 0; i < this.roi.size(); i++) {
 				roiMatrix.setDouble(0, i, this.roi.get(i));
-				ctimeMatrix.setDouble(0, i,this.ctime.get(i));
+				ctimeMatrix.setDouble(0, i, this.ctime.get(i));
 			}
-			matFile.addArray("roi",roiMatrix);
+			matFile.addArray("roi", roiMatrix);
 			matFile.addArray("ctime", ctimeMatrix);
 			try {
 				Mat5.writeToFile(matFile, "roi_profile.mat");
 			} catch (IOException e) {
 				e.printStackTrace();
+			}
+
+			r += 1;
+			if (r > this.rates.length) {
+				try {
+					Process proc = Runtime.getRuntime().exec("pkill -9 -f Workload-0.0.1-jar-with-dependencies.jar");
+					proc.waitFor();
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
