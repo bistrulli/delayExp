2110b20f09e39accc8eb63b372176954c0550e87
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-10-24 21:52:40 +0200

    minor

--- matPlotu/plotcharts.m
+++ matPlotu/plotcharts.m
@@ -4,6 +4,17 @@ N1data=load("../N1/N1out.mat");
 N2data=load("../N2/N2out.mat");
 Wdata=load("../Workload/roi_profile.mat");
 
+Wdata.roi=[300,Wdata.roi(1,1:end-1)];
+rates=[];
+for i=1:size(Wdata.roi,2)
+    if(i==1)
+        rates=[rates,repmat(Wdata.roi(i),1,sum(N1data.ctime<Wdata.ctime(1,i)))];
+    else
+        rates=[rates,repmat(Wdata.roi(i),1,sum(N1data.ctime>Wdata.ctime(1,i-1) ...
+                          & N1data.ctime<Wdata.ctime(1,i)) )];
+    end
+end
+
 startTime=0;
 
 n1d=N1data.rt;
@@ -22,9 +33,6 @@ stairs(n1d);
 plot(n1Cum);
 yline(N1req,'-.');
 e1=abs(n1Cum(1,end)-N1req)*100/N1req;
-% for i=1:size(Wdata.ctime)
-%     
-% end
 
 figure
 hold on
@@ -43,3 +51,8 @@ figure
 hold on
 title("N2_{core}")
 stairs(N1data.core);
+
+figure
+hold on
+title("roi")
+stairs(rates);
