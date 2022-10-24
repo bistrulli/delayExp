9ec662a0e1b21bd3fb52fbdbf72e07a8fb90647f
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-10-24 13:30:49 +0200

    minior

--- matPlotu/plotcharts.m
+++ matPlotu/plotcharts.m
@@ -1,5 +1,23 @@
 clear
 
 N1data=load("../N1/N1out.mat");
-Wdata=load("../Workload/roi_profile.mat");
+N2data=load("../N2/N2out.mat");
+%Wdata=load("../Workload/roi_profile.mat");
 
+n1d=N1data.rt(1:10:end);
+n2d=N2data.rt(1:10:end);
+
+figure
+hold on
+title("N1")
+stairs(n1d);
+plot(cumsum(n1d)./linspace(1,size(n1d,2),size(n1d,2)));
+yline(0.6,'-.','Threshold');
+
+
+figure
+hold on
+title("N2")
+stairs(N2data.rt);
+plot(cumsum(N2data.rt)./linspace(1,size(N2data.rt,2),size(N2data.rt,2)));
+yline(0.25,'-.','Threshold');
