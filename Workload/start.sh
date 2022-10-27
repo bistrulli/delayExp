adca8eebd492e745451799eb07615d4ee4adc379
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-10-27 18:49:44 +0200

    data

--- Workload/start.sh
+++ Workload/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar ./target/Workload-0.0.1-jar-with-dependencies.jar --initPop 1 --tier1Host localhost --aRate 20
+java -jar ./target/Workload-0.0.1-jar-with-dependencies.jar --initPop 30 --tier1Host localhost --aRate 100
