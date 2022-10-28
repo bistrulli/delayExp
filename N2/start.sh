d59c05ca64add994976f7af7f3ba2b6cb44a3696
emilio-imt <emilio-imt@MacBook-Pro.homenet.telecomitalia.it>
2022-10-28 23:25:08 +0200

    minor

--- N2/start.sh
+++ N2/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+redis-cli set N2_sla 0.15
 java -jar ./target/N2-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3200 --name N2 --alfa 0.95 --tgt 0.15 --nr 20  --dbHost localhost 
