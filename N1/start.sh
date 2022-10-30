10e1fd5c36c59c6da4a7981878336dde49ddc23e
emilio-imt <emilio-imt@MacBook-Pro.homenet.telecomitalia.it>
2022-10-30 13:29:04 +0100

    minor

--- N1/start.sh
+++ N1/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 redis-cli set N1_sla 0.25
-java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa 0.95 --tgt 0.25 --nr 20 --dbHost localhost
+alfa=0.9
+nr=20
+java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa $alfa --tgt 0.25 --nr $nr --dbHost localhost
--- N2/start.sh
+++ N2/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 redis-cli set N2_sla 0.15
-java -jar ./target/N2-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3200 --name N2 --alfa 0.95 --tgt 0.15 --nr 20  --dbHost localhost 
+alfa=0.9
+nr=20
+java -jar ./target/N2-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3200 --name N2 --alfa $alfa --tgt 0.15 --nr $nr  --dbHost localhost 
