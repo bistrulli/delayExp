dc25cc47af7851c4756b854d9e23efbacaaca066
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-11-03 19:15:54 +0100

    minor

--- N1/start.sh
+++ N1/start.sh
@@ -4,5 +4,5 @@ sudo echo "100000 100000" > /sys/fs/cgroup/N1/e1/cpu.max
 
 redis-cli set N1_sla 0.25
 alfa=0.95
-nr=100
+nr=10
 java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa $alfa --tgt 0.25 --nr $nr --dbHost localhost
