59d27088d593ade978eb96adcd10c05646fc1104
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-11-04 09:53:51 +0100

    added install script

--- install_maven.sh
+++ install_maven.sh
@@ -2,7 +2,7 @@
 
 cd 
 wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
-tar -xfv apache-maven-3.8.6-bin.tar.gz
+tar -xvf apache-maven-3.8.6-bin.tar.gz
 
 echo "export JAVA_HOME=/lib/jvm/java-16-openjdk-amd64/" >> .profile
 echo "export M2_HOME=~/apache-maven-3.8.6" >> .profile
