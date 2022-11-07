ebbd3dc6ad5eefc31f1fc58f3c793eec67f4025c
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-11-07 10:29:03 +0100

    minor

--- N2/start.sh
+++ N2/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo echo "max" > /sys/fs/cgroup/N2/e1/cpu.max
+sudo echo "100000 100000" > /sys/fs/cgroup/N2/e1/cpu.max
 
 redis-cli set N2_sla 0.15
-alfa=0.7
+alfa=0.975
 nr=10
 taskset -c 1-31 java -jar ./target/N2-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3200 --name N2 --alfa $alfa --tgt 0.15 --nr $nr  --dbHost localhost 
