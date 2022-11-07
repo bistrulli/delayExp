f44738045b3cee50584bd743fb4aaa742539e31f
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-11-07 16:53:00 +0100

    exp2

--- Workload/src/main/java/app/Simlation.java
+++ Workload/src/main/java/app/Simlation.java
@@ -35,7 +35,7 @@ public class Simlation implements Runnable {
 		this.rates = new Long[] { 150l, 150l, 150l};
 		// this.rates = new Long[] { 10l,5l,30l,30l};
 		//this.slas=new Double[]{0.35,0.55,0.20,0.60,0.25};
-		this.slas=new Double[]{0.005,0.25,0.25};
+		this.slas=new Double[]{0.100,0.25,0.25};
 		this.j = new Jedis("localhost");
 	}
 
