8039e367d14dcfc034757a8ace4ed82f2b8767ae
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-11-09 11:49:19 +0100

    minor

--- N1/start.sh
+++ N1/start.sh
@@ -2,7 +2,7 @@
 
 sudo echo "max" > /sys/fs/cgroup/N1/e1/cpu.max
 
-redis-cli set N1_sla 0.25
+redis-cli set N1_sla 0.24
 alfa=0.90
 nr=10
-taskset -c 1-31 java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa $alfa --tgt 0.25 --nr $nr --dbHost localhost
+taskset -c 1-31 java -jar ./target/N1-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3100 --name N1 --alfa $alfa --tgt 0.24 --nr $nr --dbHost localhost
--- N2/start.sh
+++ N2/start.sh
@@ -2,7 +2,7 @@
 
 sudo echo "max" > /sys/fs/cgroup/N2/e1/cpu.max
 
-redis-cli set N2_sla 0.15
+redis-cli set N2_sla 0.14
 alfa=0.90
 nr=10
-taskset -c 1-31 java -jar ./target/N2-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3200 --name N2 --alfa $alfa --tgt 0.15 --nr $nr  --dbHost localhost 
+taskset -c 1-31 java -jar ./target/N2-0.0.1-jar-with-dependencies.jar --cpuEmu 0 --cgv2 1 --port 3200 --name N2 --alfa $alfa --tgt 0.14 --nr $nr  --dbHost localhost 
